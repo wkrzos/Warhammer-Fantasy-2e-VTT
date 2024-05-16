0fa26399364b4af82caa892ec3cae4631bb128f6
Versim <wojciechkrzos67@gmail.com>
2024-05-16 13:48:21 +0200

    chore: remove redundant comments for greater readability

--- frontend/util/dropdown.py
+++ frontend/util/dropdown.py
@@ -16,25 +16,15 @@ class Dropdown:
             options (list): A list of options to display in the dropdown.
             default_text (str): The default text to display when no option is selected. Defaults to 'Select'.
         """
-        # Rectangle representing the dropdown area
         self.rect = pygame.Rect(x, y, w, h)
-        # Color of the dropdown border and text
         self.color = pygame.Color('black')
-        # Font used to render the text
         self.font = font
-        # Options available in the dropdown
         self.options = options
-        # The text currently displayed in the dropdown
         self.text = default_text
-        # Rendered surface of the current text
         self.txt_surface = self.font.render(self.text, True, self.color)
-        # Dropdown active state (True if expanded, False otherwise)
         self.active = False
-        # Dropdown selection state (True if an option is selected, False otherwise)
         self.selected = False
-        # Rectangles representing each option in the dropdown
         self.dropdown_rects = [pygame.Rect(x, y + h * (i + 1), w, h) for i in range(len(options))]
-        # Rendered surfaces of each option
         self.option_surfaces = [self.font.render(option, True, self.color) for option in options]
 
     def handle_event(self, event):
