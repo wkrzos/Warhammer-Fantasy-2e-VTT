a7c995f020633ab3a9beb5bdb54239c9f562164c
Filpus <fil-pus@wp.pl>
2024-05-29 14:23:39 +0200

    fix - try repair music

--- model/music_player_model.py
+++ model/music_player_model.py
@@ -14,23 +14,30 @@ class MusicPlayerModel:
         return [os.path.basename(song) for song in self.music_manager.playlist]
 
     def play_music(self):
-        self.music_manager.command = MusicEventTypes.PLAY
-        self.is_playing = True
+        with self.music_manager.lock:
+            self.music_manager.command = MusicEventTypes.PLAY
+            self.is_playing = True
 
     def stop_music(self):
-        self.music_manager.command = MusicEventTypes.STOP
-        self.is_playing = False
+        with self.music_manager.lock:
+            self.music_manager.command = MusicEventTypes.STOP
+            self.is_playing = False
 
     def pause_music(self):
-        self.music_manager.command = MusicEventTypes.PAUSE
-        self.is_playing = False
+        with self.music_manager.lock:
+            self.music_manager.command = MusicEventTypes.PAUSE
+            self.is_playing = False
 
     def next_music(self):
-        self.music_manager.command = MusicEventTypes.NEXT
+        with self.music_manager.lock:
+            self.music_manager.command = MusicEventTypes.NEXT
 
     def prev_music(self):
-        self.music_manager.command = MusicEventTypes.PREVIOUS
+        with self.music_manager.lock:
+            self.music_manager.command = MusicEventTypes.PREVIOUS
 
     def select_song(self, index):
-        self.music_manager.currentIndex = index
-        self.music_manager.command = MusicEventTypes.PLAY
+        with self.music_manager.lock:
+
+            self.music_manager.currentIndex = index
+            self.music_manager.command = MusicEventTypes.PLAY
