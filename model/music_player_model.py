e21deeda40ba82df44067bc55223239ecd8e0973
Filpus <fil-pus@wp.pl>
2024-05-29 14:34:17 +0200

    fix - succesfully repaired music

--- model/music_player_model.py
+++ model/music_player_model.py
@@ -1,11 +1,16 @@
 import os
+import time
+
 from backend.musicManager.music_manager import MusicManager, MusicEventTypes
 from threading import Thread
 
-class MusicPlayerModel:
+from backend.paterns.observer.observer import Observer
+
+
+class MusicPlayerModel():
     def __init__(self, music_folder_path):
         self.music_manager = MusicManager(music_folder_path)
-        self.music_thread = Thread(target=self.music_manager.run)
+        self.music_thread = Thread(target=self.music_manager.run, daemon=True)
         self.music_thread.start()
         self.is_playing = False
         self.current_index = 0
@@ -15,18 +20,21 @@ class MusicPlayerModel:
 
     def play_music(self):
         with self.music_manager.lock:
-            self.music_manager.command = MusicEventTypes.PLAY
-            self.is_playing = True
+            self.music_manager.command = MusicEventTypes.UNPAUSE
+        self.is_playing = True
 
     def stop_music(self):
         with self.music_manager.lock:
-            self.music_manager.command = MusicEventTypes.STOP
-            self.is_playing = False
+            self.music_manager.command = MusicEventTypes.PAUSE
+        self.is_playing = False
 
     def pause_music(self):
         with self.music_manager.lock:
+            self.music_manager.command = MusicEventTypes.REWIND
+        time.sleep(0.03)
+        with self.music_manager.lock:
             self.music_manager.command = MusicEventTypes.PAUSE
-            self.is_playing = False
+        self.is_playing = False
 
     def next_music(self):
         with self.music_manager.lock:
@@ -36,8 +44,11 @@ class MusicPlayerModel:
         with self.music_manager.lock:
             self.music_manager.command = MusicEventTypes.PREVIOUS
 
-    def select_song(self, index):
-        with self.music_manager.lock:
 
-            self.music_manager.currentIndex = index
+    def select_song_on_list(self, item):
+        with self.music_manager.lock:
+            self.music_manager.currentIndex = item
+            self.music_manager.command = MusicEventTypes.PAUSE
+        time.sleep(0.03)
+        with self.music_manager.lock:
             self.music_manager.command = MusicEventTypes.PLAY
