74913bf90c477845eeab58ae01546bc19586bd3a
Wojciech Krzos <w.krzos.contact@gmail.com>
2024-05-31 00:17:43 +0200

    feat: add basic chat with temp users

--- controller/chat_controller.py
+++ controller/chat_controller.py
@@ -2,7 +2,21 @@ class ChatController:
     def __init__(self, model, view):
         self.model = model
         self.view = view
+        self.connect_signals()
 
-    def add_message(self, message):
-        self.model.add_message(message)
+    def connect_signals(self):
+        self.view.send_button.clicked.connect(self.handle_send_message)
+
+    def handle_send_message(self):
+        nickname = self.view.nickname_input.text().strip()
+        if not nickname:
+            self.view.nickname_input.setFocus()
+            return
+        message = self.view.input_field.text().strip()
+        if message:
+            self.add_message(nickname, message)
+            self.view.input_field.clear()
+
+    def add_message(self, nickname, message):
+        self.model.add_message(nickname, message)
         self.view.update_chat(self.model.get_messages())
